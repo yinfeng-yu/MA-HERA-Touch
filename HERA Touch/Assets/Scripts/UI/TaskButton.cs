99d68dd993ce87403853838137bd80d47670c326
yinfeng-yu <shawnyuyf@163.com>
2023-04-07 21:21:18 +0200

    4.7

--- HERA Touch/Assets/Scripts/UI/TaskButton.cs
+++ HERA Touch/Assets/Scripts/UI/TaskButton.cs
@@ -1,10 +1,13 @@
 using System;
 using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TaskButton : Expandable
 {
     public TaskData taskData;
+
+    public Button assignButton;
 
     protected override void Start()
     {
@@ -12,6 +15,8 @@ public class TaskButton : Expandable
 
         EventManager.instance.taskBubbleEvents.clicked += ResetExpandable;
         SwipeManager.instance.swipeUp += OnSwipeUp;
+
+        assignButton.onClick.AddListener(() => { AssignTask(); });
     }
 
     protected override void OnClicked()
@@ -19,17 +24,32 @@ public class TaskButton : Expandable
         base.OnClicked();
         EventManager.instance.taskButtonEvents.Clicked();
         EventManager.instance.taskButtonEvents.Selected(_selected);
+
+        if (GetComponent<Animator>().GetBool("inspected")) GetComponent<Animator>().SetBool("inspected", false);
     }
 
     void OnSwipeUp()
     {
         if (_selected) 
         {
-            Debug.Log("Task Added!");
-            AgentManager.instance.robotAgents[AgentManager.instance.currentAgent].AddTask(new Task(taskData));
+            // Debug.Log("Task Added!");
+            // AgentManager.instance.robotAgents[AgentManager.instance.currentAgent].AddTask(new Task(taskData));
 
-            ResetExpandable();
+            // ResetExpandable();
+
+            Debug.Log("Task Inspected");
+            GetComponent<Animator>().SetBool("inspected", true);
+
         }
+    }
+
+    void AssignTask()
+    {
+        Debug.Log("Task Added!");
+        AgentManager.instance.robotAgents[AgentManager.instance.currentAgent].AddTask(new Task(taskData));
+
+        GetComponent<Animator>().SetBool("inspected", false);
+        GetComponent<Animator>().SetBool("selected", false);
     }
 
 }
